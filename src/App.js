14471aa11ee8f08032a007df98971f225ef64a5d
Seyfedin Navruzov <91982086+lfnickname@users.noreply.github.com>
2022-02-10 22:51:50 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styles from './app.module.css';
+import { chess_figures } from './data';
 
 function App() {
+
+  const [items, setItems] = useState(chess_figures)
+  const [currentItem, setCurrentItem] = useState(null)
+  const pulled = items.filter(item => item.position!==null)
+
+  let file = new File([JSON.stringify(items)], "file.txt", {
+      type: "text/plain",
+      });
+
+  const sortItems = (a, b) => {
+    if (a.position > b.position) {return 1}
+    else {return -1}
+  }
+
+  function dragStartHandler(e, card){
+    setCurrentItem(card)
+  }
+
+  function dragOverHandler(e){
+    e.preventDefault()
+  }
+
+  function dragDropHandler(e, card){
+    e.preventDefault()
+    e.stopPropagation()
+    if (currentItem.position === null) {
+      const targetPosition = card.position
+      setItems(items.map(item => {
+        if (item.id === card.id) {item.position = null; return item}
+        else if (item.id === currentItem.id) {item.position = targetPosition; return item}
+        else return item
+      }))
+    }
+    else {
+      const targetPosition = card.position
+      const itemPosition = currentItem.position
+      setItems(items.map(item => {
+        if (item.id === card.id) {item.position = itemPosition; return item}
+        else if (item.id === currentItem.id) {item.position = targetPosition; return item}
+        else return item
+      }))
+    }
+  }
+
+  function boardDragOverHandler (e) {
+    e.preventDefault()
+  }
+
+  function boardDragDropHandler (e, item) {
+    e.preventDefault()
+    if (pulled.length===0) {
+      setItems(items.map(item => {if (item.id === currentItem.id) {item.position = 0; return item} else return item}))
+    }
+    else {
+      setItems(items.map(item => {if (item.id === currentItem.id) {item.position = pulled.length; return item} else return item}))
+    }
+  }
+
+  function returnDragOverHandler (e) {
+    e.preventDefault()
+  }
+
+  function returnDragDropHandler (e) {
+    e.preventDefault()
+    setItems(items.map(item => {if (item.id === currentItem.id) {item.position = null; return item} else return item}))
+  }
+
+
+  function showFile(file) {
+    let reader = new FileReader()
+    reader.readAsText(file)
+    reader.onload = function() {
+      setItems(JSON.parse(reader.result))
+    };
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <div 
+        className={styles.items_wrapper}
+        onDragOver={(e)=> returnDragOverHandler(e)}
+        onDrop={(e)=> returnDragDropHandler(e)}>
+        {items.filter(item => item.position===null).map((item) => {
+          return (
+          <div
+            className={styles.item}
+            key={item.id}
+            onDragStart={(e)=> dragStartHandler(e, item)}
+            draggable={true}>
+          <img width={50} height={50} src={item.url}/>
+        </div>
+        )})}
+      </div>
+      <div className={styles.board}           
+        onDragOver={(e)=> boardDragOverHandler(e)}
+        onDrop={(e)=> boardDragDropHandler(e)}>
+        <div className={styles.items_wrapper_bot}>
+        {pulled.sort(sortItems).map((item) => {
+          return (
+          <div
+            key={item.id}
+            onDragStart={(e)=> dragStartHandler(e, item)}
+            onDragOver={(e)=> dragOverHandler(e, item)}
+            onDrop={(e)=> dragDropHandler(e, item)}
+            draggable={true}>
+            <img className={styles.item} width={50} height={50}  src={item.url}/>
+          </div>
+        )})}
+        </div>
+      </div>
+      <div className={styles.buttons_wrapper}>
+        <a download={file.name} href={URL.createObjectURL(file)}>Download preset</a>
+        <div className={styles.input_wrapper}>
+          <label for="file-upload" className={styles.custominput}>
+              Upload preset
+          </label>
+          <input id="file-upload" type="file" onChange={(e)=> showFile(e.target.files[0])}/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const chess_figures = [
+    {
+      id: 1,
+      position: null,
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Chess_tile_kl.svg/1024px-Chess_tile_kl.svg.png'
+    },
+    {
+      id: 2,
+      position: null,
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Chess_tile_kd.svg/1200px-Chess_tile_kd.svg.png'
+    },
+    {
+      id: 3,
+      position: null,
+      url: 'https://svgsilh.com/png-1024/151548.png'
+    },
+    {
+      id: 4,
+      position: null,
+      url: 'https://www.pinclipart.com/picdir/big/18-186849_white-queen-chess-queen-icon-clipart.png'
+    },
+    {
+      id: 5,
+      position: null,
+      url: 'https://freesvg.org/img/portablejim-Chess-tile-Rook-1.png'
+    },
+    {
+      id: 6,
+      position: null,
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Chess_tile_rl.svg/1200px-Chess_tile_rl.svg.png'
+    },
+    {
+      id: 7,
+      position: null,
+      url: 'https://icon-library.com/images/bishop-icon/bishop-icon-9.jpg'
+    },
+    {
+      id: 8,
+      position: null,
+      url: 'http://www.clker.com/cliparts/0/b/a/3/12205465671996376634portablejim_Chess_tile_-_Bishop_1.svg.hi.png'
+    },
+    {
+      id: 9,
+      position: null,
+      url: 'https://cdn2.iconfinder.com/data/icons/rpg-basic-set-2/512/chess_knight-512.png'
+    },
+    {
+      id: 10,
+      position: null,
+      url: 'https://icon-library.com/images/chess-knight-icon/chess-knight-icon-2.jpg'
+    },
+  ]
